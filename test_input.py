86c019795cc0185586d6df153eca05d7ad4b568b
weekendone <daniel@daniels-iMac.local>
2015-07-26 20:28:50 -0500

    commit

---
+++ test_input.py
@@ -0,0 +1,23 @@
+x = int(input("Enter the first number: "))
+
+
+
+y = 1
+
+for y in range(1, x):
+    print("{0}".format(y))
+
+
+
+
+z = 1
+
+r = int(input("Enter the first number: "))
+
+for z in range (1, r):
+    print("{0}".format(z), end=" ")
+
+
+
+
+
